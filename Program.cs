30a8e39168ec3098337682fb6d4ac8eb743b0922
Insung Park <sleepwithoutadream@gmail.com>
2025-07-14 09:18:11 +0900

    1

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.Runtime.CompilerServices;
 
 class TextRPG
 {
+    private List<Item> playerItem = new List<Item>();
+    private List<Item> storeItem = new List<Item>();
     private int Lvl;
     private string Chad;
     private int attack;
@@ -32,6 +34,7 @@ class TextRPG
 
     private void StartGame()
     {
+        AddStoreItem();
         while (true)
         {
             MainMenu();
@@ -136,22 +139,32 @@ class TextRPG
         public bool isOwned = false;
         public bool isEquipped = false;
 
-        public Item(string name, string info, int effect, int price)
+        public Item(string name, string info, int effect, int price, bool isOwned, bool isEquipped)
         {
             this.name = name;
             this.info = info;
             this.effect = effect;
             this.price = price;
+            this.isOwned = isOwned;
+            this.isEquipped = isEquipped;
         }
+    }
+
+    private void AddStoreItem()
+    {
+        storeItem.Add(new Item("수련자 갑옷", "수련에 도움을 주는 갑옷입니다.", 5, 1000, false, false));
+        storeItem.Add(new Item("무쇠갑옷", "무쇠로 만들어져 튼튼한 갑옷입니다.", 9, 0, false, false)); //구매완료 = 0 G
+        storeItem.Add(new Item("스파르타의 갑옷", "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 15, 3500, false, false));
+        storeItem.Add(new Item("낡은 검", "쉽게 볼 수 있는 낡은 검 입니다.", 2, 600, false, false));
+        storeItem.Add(new Item("청동 도끼", "어디선가 사용됐던거 같은 도끼입니다.", 5, 1500, false, false));
+        storeItem.Add(new Item("스파르타의 창", "스파르타의 전사들이 사용했다는 전설의 창입니다.", 7, 0, false, false)); //구매완료 = 0 G
     }
 
     private void ManageItem()
     {
-        bool isEquipped1 = false;
-        bool isEquipped2 = false;
-        bool isEquipped3 = false;
-        int item1 = 5;
-
+        playerItem.Add(new Item("무쇠갑옷", "무쇠로 만들어져 튼튼한 갑옷입니다.", 5, 1000, true, false));
+        playerItem.Add(new Item("스파르타의 창", "스파르타의 전사들이 사용했다는 전설의 창입니다.", 7, 0, true, false));
+        playerItem.Add(new Item("낡은 검", "쉽게 볼 수 있는 낡은 검 입니다.", 2, 600, true, false));
 
         while (true)
         {
@@ -159,9 +172,9 @@ class TextRPG
             Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
             Console.WriteLine();
             Console.WriteLine("[아이템 목록]");
-            Console.WriteLine($"- 1 {(isEquipped1 ? "[E]" : "")}무쇠갑옷      | 방어력 +5 | 무쇠로 만들어져 튼튼한 갑옷입니다.");
-            Console.WriteLine($"- 2 {(isEquipped2 ? "[E]" : "")}스파르타의 창  | 공격력 +7 | 스파르타의 전사들이 사용했다는 전설의 창입니다.");
-            Console.WriteLine($"- 3 {(isEquipped3 ? "[E]" : "")}낡은 검         | 공격력 +2 | 쉽게 볼 수 있는 낡은 검 입니다.");
+            Console.WriteLine($"- 1 {(playerItem[1].isEquipped ? "[E]" : "")}{playerItem[1].name}      | 방어력 +{playerItem[1].effect} | {playerItem[1].info} ");
+            Console.WriteLine($"- 2 {(playerItem[2].isEquipped ? "[E]" : "")}{playerItem[2].name}  | 공격력 +{playerItem[1].effect}  | {playerItem[1].info}");
+            Console.WriteLine($"- 3 {(playerItem[3].isEquipped ? "[E]" : "")}{playerItem[3].name}         | 공격력 +{playerItem[1].effect}  | {playerItem[1].info}");
             Console.WriteLine();
             Console.WriteLine("0. 나가기");
             Console.WriteLine();
@@ -170,10 +183,10 @@ class TextRPG
 
             if (input == "1")
             {
-                if (isEquipped1 == false)
+                if (playerItem[1].isEquipped == false)
                 {
-                    isEquipped1 = true;
-                    addArmor += 5;
+                    playerItem[1].isEquipped = true;
+                    addArmor += playerItem[1].effect;
                     armor += addArmor;
 
                     Console.WriteLine("방어력 + 5.");
@@ -181,9 +194,9 @@ class TextRPG
 
                 else
                 {
-                    isEquipped1 = false;
+                    playerItem[1].isEquipped = false;
                     armor -= addArmor;
-                    addArmor -= 5;
+                    addArmor -= playerItem[1].effect;
                     Console.WriteLine("방어력 - 5.");
 
                 }
@@ -191,38 +204,38 @@ class TextRPG
 
             else if (input == "2")
             {
-                if (isEquipped2 == false)
+                if (playerItem[2].isEquipped == false)
                 {
-                    isEquipped2 = true;
-                    addAttack += 7;
+                    playerItem[2].isEquipped = true;
+                    addAttack += playerItem[2].effect;
                     attack += addAttack;
                     Console.WriteLine("공격력 + 7.");
                 }
 
                 else
                 {
-                    isEquipped2 = false;
+                    playerItem[2].isEquipped = false;
                     attack -= addAttack;
-                    addAttack -= 7;
+                    addAttack -= playerItem[2].effect;
                     Console.WriteLine("방어력 - 7.");
                 }
             }
 
             else if (input == "3")
             {
-                if (isEquipped3 == false)
+                if (playerItem[3].isEquipped == false)
                 {
-                    isEquipped3 = true;
-                    addAttack += 2;
+                    playerItem[3].isEquipped = true;
+                    addAttack += playerItem[3].effect;
                     attack += addAttack;
                     Console.WriteLine("공격력 + 2.");
                 }
 
                 else
                 {
-                    isEquipped3 = false;
+                    playerItem[3].isEquipped = false;
                     attack -= addAttack;
-                    addAttack -= 2;
+                    addAttack -= playerItem[3].effect;
                     Console.WriteLine("방어력 - 2.");
                 }
             }
@@ -244,12 +257,12 @@ class TextRPG
             Console.WriteLine($"{gold} G");
             Console.WriteLine();
             Console.WriteLine("[아이템 목록]");
-            Console.WriteLine("- 1 수련자 갑옷    | 방어력 +5  | 수련에 도움을 주는 갑옷입니다.             |  1000 G");
-            Console.WriteLine("- 2 무쇠갑옷      | 방어력 +9  | 무쇠로 만들어져 튼튼한 갑옷입니다.           |  구매완료");
-            Console.WriteLine("- 3 스파르타의 갑옷 | 방어력 +15 | 스파르타의 전사들이 사용했다는 전설의 갑옷입니다.|  3500 G");
-            Console.WriteLine("- 4 낡은 검      | 공격력 +2  | 쉽게 볼 수 있는 낡은 검 입니다.            |  600 G");
-            Console.WriteLine("- 5 청동 도끼     | 공격력 +5  |  어디선가 사용됐던거 같은 도끼입니다.        |  1500 G");
-            Console.WriteLine("- 6 스파르타의 창  | 공격력 +7  | 스파르타의 전사들이 사용했다는 전설의 창입니다. |  구매완료");
+            Console.WriteLine($"- 1 {storeItem[1].name}    | 방어력 +{storeItem[1].effect}  | {storeItem[1].info}             |  {(storeItem[1].isOwned ? "판매완료" : storeItem[1].price)} G");
+            Console.WriteLine($"- 2 {storeItem[2].name}      | 방어력 +{storeItem[2].effect} | {storeItem[2].info}           | {(storeItem[2].isOwned ? "판매완료" : storeItem[2].price)} G");
+            Console.WriteLine($"- 3 {storeItem[3].name} | 방어력 +{storeItem[3].effect} | {storeItem[3].info}|  {(storeItem[3].isOwned ? "판매완료" : storeItem[3].price)} G");
+            Console.WriteLine($"- 4 {storeItem[4].name}      | 공격력 +{storeItem[4].effect}  | {storeItem[4].info}            |  {(storeItem[4].isOwned ? "판매완료" : storeItem[4].price)} G");
+            Console.WriteLine($"- 5 {storeItem[5].name}     | 공격력 +{storeItem[5].effect}  |  {storeItem[5].info}        |  {(storeItem[5].isOwned ? "판매완료" : storeItem[5].price)} G");
+            Console.WriteLine($"- 6 {storeItem[6].name}  | 공격력 +{storeItem[6].effect}  | {storeItem[6].info} |  {(storeItem[6].isOwned ? "판매완료" : storeItem[6].price)} G");
             Console.WriteLine();
             Console.WriteLine("1. 아이템 구매");
             Console.WriteLine("0. 나가기");
@@ -258,7 +271,14 @@ class TextRPG
 
             if (input == "1")
             {
-                //if (gold >= 1000) ;
+                if (playerItem[1].isOwned == true) Console.WriteLine("이미 구매한 아이템입니다.");
+
+                if (gold < storeItem[1].price) Console.WriteLine("Gold 가 부족합니다.");
+
+                if (playerItem[1].isOwned != true && gold > storeItem[1].price)
+                {
+                    playerItem.Add(new Item("수련자 갑옷", "수련에 도움을 주는 갑옷입니다.", 5, 1000, true, false));
+                }
             }
 
             else if (input == "2")
